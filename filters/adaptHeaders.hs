d3059075d35cb136882a8bdd2f17541519b75cb0
cgroll <groll.christian.edu@gmail.com>
2015-04-14 23:43:14 +0200

    header automatic link removed

--- filters/adaptHeaders.hs
+++ filters/adaptHeaders.hs
@@ -6,6 +6,6 @@ import Text.Pandoc.JSON
 -- transform h4 to new paragraph with strong text
 main :: IO ()
 main = toJSONFilter adaptHeaders
-  where adaptHeaders (Header n _ xs) | n == 2 = (Header 3 ("section", [], []) xs)
+  where adaptHeaders (Header n _ xs) | n == 2 = (Header 3 ([], [], []) xs)
      	                               | n == 4 = (Para [Strong xs])
         adaptHeaders x = x
